7e2ce46519a3931a4d5ffdacd737d11556d0e887
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-11-30 22:26:41 +0530

    Learned interaction with multiple components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 import Heading from './components/Heading';
+import Card from './components/Card';
 
 function App(props) {
   return (
     <div className='App'>
-      <Heading firstName='Agnii!'/>
-
-      <Heading firstName='Rahul'/>
-      <h1>{}</h1>
+      <Heading firstName='Agnii!' />
+      {/* <Heading firstName='Rahul'/> */}
+      <h1>
+      {"Task: Add three Card elements"}
+      <Card h2="First card's h2" h3= "First card's h3" />
+      <Card h2 = "Second card's h2" h3 ="Senond card's h3" />
+      <Card h2 = "Third card's h2" h3 ="Third card's h3" />
+      </h1>
     </div>
   )
 }
