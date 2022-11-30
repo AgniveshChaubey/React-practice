e0039fd3fa0cab151153c44a18fadb3e54e5f08c
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-11-30 16:25:56 +0530

    Seperated the components and launched first app

---
+++ src/components/Promo.js
@@ -0,0 +1,12 @@
+function Promo(){
+    <div className="promo-section">
+        <div>
+            <h1>
+                Don't miss this deal!
+                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
+            </h1>
+        </div>
+    </div>
+}
+
+export default Promo;
