0493fca1eb93b99eef7633ab875809b4cd812bad
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-12-01 20:30:10 +0530

    performed prop drilling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import './App.css';
-import {Btn, Btn2} from './components/Card';
+import { Btn, Btn2 } from './components/Card';
 import { ModeToggler } from './components/Card';
-import { RegisterForm, TextInputWithFocusButton } from './components/InputFunction'; 
+import { RegisterForm, TextInputWithFocusButton } from './components/InputFunction';
 import Heading from './components/Heading';
 import Card from './components/Card';
+import Main from './components/Main';
 
 function App(props) {
   return (
     <div className='App'>
-      <Heading firstName='Agnii!' />
+      {/* <Heading firstName='Agnii!' /> */}
       {/* <Heading firstName='Rahul'/> */}
       <h1>
-      {/* {"Task: Add a button and handle a click event!"} */}
+        {/* {"Task: Add a button and handle a click event!"} */}
       </h1>
-      <RegisterForm />
-      <TextInputWithFocusButton />
+      {/* <RegisterForm />
+      <TextInputWithFocusButton /> */}
       {/* <Btn2 /> */}
       {/* <InputFunction /> */}
       {/* <Card h2="First card's h2" h3= "First card's h3" /> */}
       {/* <Btn /> */}
-      <ModeToggler />
+      {/* <ModeToggler /> */}
+      <Main msg="I passed through the Header and the Wrapper and I reached the Button component" />
       {/* <Card h2 = "Second card's h2" h3 ="Senond card's h3" />
       <Card h2 = "Third card's h2" h3 ="Third card's h3" /> */}
     </div>
-  )
-}
+  );
+};
 
 export default App;
