f37a225b1d814d45946691de523e678ca50f8541
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-12-01 16:31:52 +0530

    useState 2nd example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {Btn, Btn2} from './components/Card';
 import { ModeToggler } from './components/Card';
-import { InputFunction } from './components/InputFunction'; 
+import { RegisterForm } from './components/InputFunction'; 
 import Heading from './components/Heading';
 import Card from './components/Card';
 
@@ -11,12 +11,13 @@ function App(props) {
       <Heading firstName='Agnii!' />
       {/* <Heading firstName='Rahul'/> */}
       <h1>
-      {"Task: Add a button and handle a click event!"}
+      {/* {"Task: Add a button and handle a click event!"} */}
       </h1>
-      <Btn2 />
-      <InputFunction />
-      <Card h2="First card's h2" h3= "First card's h3" />
-      <Btn />
+      <RegisterForm />
+      {/* <Btn2 /> */}
+      {/* <InputFunction /> */}
+      {/* <Card h2="First card's h2" h3= "First card's h3" /> */}
+      {/* <Btn /> */}
       <ModeToggler />
       {/* <Card h2 = "Second card's h2" h3 ="Senond card's h3" />
       <Card h2 = "Third card's h2" h3 ="Third card's h3" /> */}
--- src/components/InputFunction.js
+++ src/components/InputFunction.js
@@ -1,19 +1,42 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-export function InputFunction(){
-    const [inputText, setText] = useState('hello');
+export function RegisterForm() {
+    const [form, setForm] = useState({
+        firstName: 'Agnivesh',
+        lastName: 'Chaubey',
+        email: 'agn@gmail.com',
+    });
 
-    function handleChange(e){
-        setText(e.target.value);
+    function handleChange(e) {
+        setForm({ ...form, fiestName: e.target.value });
     };
 
-    return(
+    return (
         <div>
-            <input value={inputText} onChange={handleChange} />
-            <p>You typed: {inputText}</p>
-            <button onClick={()=>{ setText('hello')}} >
-                Reset
-            </button>
+            <label> First name:
+                <input value={form.firstName} onChange={handleChange} />
+            </label>
+            <label>
+                Last name :
+                <input value={form.lastName} onChange={(e) => {
+                    setForm({...form, lastName: e.target.value });
+                }}
+                />
+            </label>
+            <label>
+                <input value={form.email} onChange={(e) => {
+                    setForm({...form, email: e.target.value });
+                }}
+                />
+            </label>
+            <p>
+                {form.firstName}{}
+                {form.lastName}{}
+                ({form.email})
+            </p>
         </div>
     );
 };
+
+
+
