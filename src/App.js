0b6edd9fcd9d2dd30a60cbf1cf006c5ffbfd92a7
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-12-02 22:49:39 +0530

    function created for controlling audio

--- src/App.js
+++ src/App.js
@@ -21,9 +21,33 @@ function App(props) {
     { fruitName: 'plum', id: 3 },
   ]);
 
-  function handleChange() {
 
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+  function toddle1() {
+    if (bird1.paused) {
+      bird1.play();
+    }
+    else {
+      bird1.pause();
+    }
   }
+
+  function toggle2() {
+    if (bird2.paused) {
+      bird2.play();
+    }
+    else {
+      bird2.pause();
+    }
+  }
+
+
   return (
     <div className='App'>
 
@@ -80,6 +104,10 @@ function App(props) {
         url={'https://www.youtube.com/watch?v=3GsA60QpJz0'}
 
       /> */}
+
+
+
+
 
     </div>
   );
